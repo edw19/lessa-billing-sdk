8e1d558e1238d3a2cfdc634be75132d1fc75aa1c
Edwin Narváez <47750859+edw19@users.noreply.github.com>
2025-05-16 15:58:34 -0500

    feat: Add missing exports for access key, cedula, emission point, establishment, issue date, RUC, sequential, type environment, and type receipt

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
 import { LessaBillingSDK as LessaBillingSDKClass } from './lessa-billing'
+
 export * from './libs/consumers'
 export * from './libs/taxes'
 export * from './libs/payment-methods'
 export * from './libs/regimes'
+
+export * from './libs/access-key'
+export * from './libs/cedula'
+export * from './libs/emision-point'
+export * from './libs/establishment'
+export * from './libs/issue-date'
+export * from './libs/ruc'
+export * from './libs/sequential'
+export * from './libs/type-environment'
+export * from './libs/type-receipt'
 
 export const LessaBillingSDK = LessaBillingSDKClass
 export default LessaBillingSDKClass
