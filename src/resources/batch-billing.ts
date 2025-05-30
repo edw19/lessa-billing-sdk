0b755f2a3a3a68740c92350ee96bcd5310c38115
Edwin Narváez <47750859+edw19@users.noreply.github.com>
2025-05-30 14:37:40 -0500

    fix: Update version number in package.json to 0.0.4 and enhance invoice methods in Billing class

--- src/resources/batch-billing.ts
+++ src/resources/batch-billing.ts
@@ -1,19 +1,30 @@
 import { AxiosInstance } from 'axios'
+import { BillingInvoice } from './billing'
 
 export class BatchBilling {
     constructor(private readonly axios: AxiosInstance) { }
 
-    async sendInvoicesBatch(ruc: string, establishmentCode: number, emissionPointCode: number, invoices: any[]) {
-        const resp = await this.axios.post(`billing/batch/invoices/send/${ruc}/${establishmentCode}/${emissionPointCode}`, invoices)
+    async generateInvoicesBatch(ruc: string, establishmentCode: number, emissionPointCode: number, invoices: BillingInvoice[]) {
+        const resp = await this.axios.post(`billing/batch/invoices/generate/${ruc}/${establishmentCode}/${emissionPointCode}`, invoices)
         return resp.data
     }
 
-    async checkAuthorizationInvoicesBatch(accessKeyBatch: string) {
-        const resp = await this.axios.post(`billing/batch/invoices/check/${accessKeyBatch}`)
+    async signInvoicesBatch(billingProcessId: string) {
+        const resp = await this.axios.post(`/billing/batch/invoices/sign/${billingProcessId}`)
         return resp.data
     }
-    
-    async batchInvoices(ruc: string, establishmentCode: number, emissionPointCode: number, invoices: any[]) {
+
+    async sendInvoicesBatch(billingProcessId: string) {
+        const resp = await this.axios.post(`billing/batch/invoices/send/${billingProcessId}`)
+        return resp.data
+    }
+
+    async checkAuthorizationInvoicesBatch(billingProcessId: string) {
+        const resp = await this.axios.post(`billing/batch/invoices/check/${billingProcessId}`)
+        return resp.data
+    }
+
+    async batchInvoices(ruc: string, establishmentCode: number, emissionPointCode: number, invoices: BillingInvoice[]) {
         try {
             await this.axios.post(`billing/batch/invoices/${ruc}/${establishmentCode}/${emissionPointCode}`, invoices, { timeout: 0 })
         } catch (error: any) {
--- src/resources/billing.ts
+++ src/resources/billing.ts
@@ -3,12 +3,16 @@ import { AxiosInstance } from 'axios'
 export class Billing {
     constructor(private axios: AxiosInstance) { }
 
+    async invoice(ruc: string, establishmentCode: number, emissionPointCode: number, invoice: BillingInvoice) {
+        const resp = await this.axios.post(`billing/invoice/${ruc}/${establishmentCode}/${emissionPointCode}`, invoice)
+        return resp.data
+    }
 
-    async generateInvoice(data: any): Promise<{
+    async generateInvoice(ruc: string, establishmentCode: number, emissionPoint: number, data: BillingInvoice): Promise<{
         accessKey: string;
         xml: string;
     }> {
-        const resp = await this.axios.post("billing/invoice/generate", data)
+        const resp = await this.axios.post(`billing/invoice/generate/${ruc}/${establishmentCode}/${emissionPoint}`, data)
         return resp.data
     }
 
@@ -26,4 +30,35 @@ export class Billing {
         const resp = await this.axios.post(`billing/invoice/check/${accessKey}`, {}, { timeout: 0 })
         return resp.data
     }
+}
+
+
+export interface BillingInvoice {
+    clientOnCompanyId: string;
+    invoiceAssociationId?: string;
+    paymentMethodCode: string;
+    details: Detail[];
+    billingReferenceDate?: Date
+}
+
+interface Detail {
+    code: string;
+    stubCode: string;
+    description: string;
+    discount: number;
+    quantity: number;
+    unitPrice: number;
+    taxes: Tax[];
+    AdditionalDetails?: AdditionalDetails[];
+}
+
+interface Tax {
+    code: string;
+    percentageCode: number;
+    fee: number;
+}
+
+interface AdditionalDetails {
+    name: string;
+    value: string;
 }
