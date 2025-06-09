488a56114a863226e41fba64c18d1a75e7024e25
Edwin Narváez <47750859+edw19@users.noreply.github.com>
2025-06-09 09:42:23 -0500

    add list taxes

--- src/libs/taxes.ts
+++ src/libs/taxes.ts
@@ -2,6 +2,21 @@ export const IVA_TAX_CODE = "2"
 export const ICE_TAX_CODE = "3"
 export const IRBPNR_TAX_CODE = "5"
 
+export const TAX_CODES = [
+    {
+        value: IVA_TAX_CODE,
+        label: "IVA"
+    },
+    {
+        value: ICE_TAX_CODE,
+        label: "ICE",
+    },
+    {
+        value: IRBPNR_TAX_CODE,
+        label: "IRBPNR"
+    }
+]
+
 
 export const IVA_PERCENTAGE_CODE_0 = 0
 export const IVA_FEE_0 = 0
@@ -38,3 +53,50 @@ export const IVA_FEE_DIFFERENTIATED_IVA3 = 3
 export const IVA_PERCENTAGE_CODE_13 = 10
 export const IVA_FEE_13 = 13
 
+export const IVA_RATES = [
+    {
+        percentageCode: IVA_PERCENTAGE_CODE_0,
+        fee: IVA_FEE_0,
+        label: "0%"
+    },
+    {
+        percentageCode: IVA_PERCENTAGE_CODE_12,
+        fee: IVA_FEE_12,
+        label: "12%"
+    },
+    {
+        percentageCode: IVA_PERCENTAGE_CODE_14,
+        fee: IVA_FEE_14,
+        label: "14%"
+    },
+    {
+        percentageCode: IVA_PERCENTAGE_CODE_15,
+        fee: IVA_FEE_15,
+        label: "15%"
+    },
+    {
+        percentageCode: IVA_PERCENTAGE_CODE_5,
+        fee: IVA_FEE_5,
+        label: "5%"
+    },
+    {
+        percentageCode: IVA_PERCENTAGE_CODE_NOT_SUBJECT_TO_TAX,
+        fee: IVA_FEE_NOT_SUBJECT_TO_TAX,
+        label: "No Objecto de Impuesto"
+    },
+    {
+        percentageCode: IVA_PERCENTAGE_CODE_IVA_EXEMPT,
+        fee: IVA_FEE_IVA_EXEMPT,
+        label: "Exento de IVA"
+    },
+    {
+        percentageCode: IVA_PERCENTAGE_CODE_DIFFERENTIATED_IVA3,
+        fee: IVA_FEE_DIFFERENTIATED_IVA3,
+        label: "IVA diferenciado3"
+    },
+    {
+        percentageCode: IVA_PERCENTAGE_CODE_13,
+        fee: IVA_FEE_13,
+        label: "13%"
+    }
+]
