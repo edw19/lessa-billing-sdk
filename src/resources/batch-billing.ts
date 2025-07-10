5ef5de4a6b33e18eb3a50fbd211faf638c51b081
Edwin Narváez <47750859+edw19@users.noreply.github.com>
2025-07-10 10:20:16 -0500

    fix names

--- src/resources/batch-billing.ts
+++ src/resources/batch-billing.ts
@@ -5,27 +5,32 @@ export class BatchBilling {
     constructor(private readonly axios: AxiosInstance) { }
 
     async generateInvoicesBatch(ruc: string, establishmentCode: number, emissionPointCode: number, invoices: BillingInvoice[]) {
-        const resp = await this.axios.post(`billing/batch/invoices/generate/${ruc}/${establishmentCode}/${emissionPointCode}`, invoices)
+        const resp = await this.axios.post(`batch-billing/invoices/generate/${ruc}/${establishmentCode}/${emissionPointCode}`, invoices)
         return resp.data
     }
 
     async signInvoicesBatch(billingProcessId: string) {
-        const resp = await this.axios.post(`/billing/batch/invoices/sign/${billingProcessId}`)
+        const resp = await this.axios.post(`/batch-billing/invoices/sign/${billingProcessId}`)
         return resp.data
     }
 
     async sendInvoicesBatch(billingProcessId: string) {
-        const resp = await this.axios.post(`billing/batch/invoices/send/${billingProcessId}`)
+        const resp = await this.axios.post(`batch-billing/invoices/send/${billingProcessId}`)
         return resp.data
     }
 
     async checkAuthorizationInvoicesBatch(billingProcessId: string) {
-        const resp = await this.axios.post(`billing/batch/invoices/check/${billingProcessId}`)
+        const resp = await this.axios.post(`batch-billing/invoices/check/${billingProcessId}`)
         return resp.data
     }
 
     async batchInvoices(ruc: string, establishmentCode: number, emissionPointCode: number, invoices: BillingInvoice[]) {
-        const resp = await this.axios.post(`billing/batch/invoices/${ruc}/${establishmentCode}/${emissionPointCode}`, invoices, { timeout: 0 })
+        const resp = await this.axios.post(`batch-billing/invoices/${ruc}/${establishmentCode}/${emissionPointCode}`, invoices, { timeout: 0 })
+        return resp.data
+    }
+
+    async getBillingProcess(ruc: string) {
+        const resp = await this.axios.get(`batch-billing/processes/${ruc}`)
         return resp.data
     }
 }
