8b7af8e71182ae3ba5ed8bc1875d3b45fa47e95e
Edwin Narváez <47750859+edw19@users.noreply.github.com>
2025-07-15 10:49:53 -0500

    return response blob

--- src/resources/reports.ts
+++ src/resources/reports.ts
@@ -4,8 +4,7 @@ export class Reports {
     constructor(private readonly axios: AxiosInstance) { }
 
     async getInvoicePdf(accessKey: string) {
-        const resp = await this.axios.get(`reports/bill/${accessKey}`, { responseType: "blob" })
-        return resp.data
+        return await this.axios.get(`reports/bill/${accessKey}`, { responseType: "blob" })
     }
 
 }
